52f7fbed67087dca92d1d90a3f540c9fd6999973
alinlinthia <alinsustika@gmail.com>
2019-06-02 23:03:36 +0700

    benerin API

---
+++ app/src/main/java/com/example/mydictionary/customAdapter.java
@@ -0,0 +1,64 @@
+package com.example.mydictionary;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class customAdapter extends RecyclerView.Adapter<customAdapter.ViewHolder> {
+
+    private Context context ;
+    private List<dataModel> model ;
+
+    public customAdapter(Context context, List<dataModel> model) {
+        this.context = context;
+        this.model = model;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder( ViewGroup parent , int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.template,parent,false);
+        ViewHolder vw = new ViewHolder(v);
+
+
+        return vw;
+    }
+
+    @Override
+    public void onBindViewHolder( ViewHolder holder, int position) {
+
+        dataModel dm = model.get(position);
+        holder.a1.setText(dm.getTy().toString());
+        holder.a2.setText(dm.getDe().toString());
+        holder.a3.setText(dm.getEx().toString());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return model.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView a1, a2, a3 ;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            a1 = (TextView)itemView.findViewById(R.id.Type);
+            a2 = (TextView)itemView.findViewById(R.id.definition);
+            a3 = (TextView)itemView.findViewById(R.id.example);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mydictionary/dataModel.java
@@ -0,0 +1,39 @@
+package com.example.mydictionary;
+
+public class dataModel {
+
+     String ty, de, ex ;
+
+    public dataModel(String ty, String de, String ex) {
+        this.ty = ty;
+        this.de = de;
+        this.ex = ex;
+    }
+
+    public dataModel() {
+    }
+
+    public String getTy() {
+        return ty;
+    }
+
+    public void setTy(String ty) {
+        this.ty = ty;
+    }
+
+    public String getDe() {
+        return de;
+    }
+
+    public void setDe(String de) {
+        this.de = de;
+    }
+
+    public String getEx() {
+        return ex;
+    }
+
+    public void setEx(String ex) {
+        this.ex = ex;
+    }
+}
